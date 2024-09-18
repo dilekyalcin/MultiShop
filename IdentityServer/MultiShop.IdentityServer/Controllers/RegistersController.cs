732d1ef9248c5b37a3000d7ace806e2a56eabb16
Dilek <dilekyal1@gmail.com>
2024-09-18 22:05:13 +0300

    feat: add identityServer

---
+++ IdentityServer/MultiShop.IdentityServer/Controllers/RegistersController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.IdentityServer.Dtos;
+using MultiShop.IdentityServer.Models;
+using System.Threading.Tasks;
+
+namespace MultiShop.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _usermanager;
+
+        public RegistersController(UserManager<ApplicationUser> usermanager)
+        {
+            _usermanager = usermanager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UserRegister(UserRegisterDto userRegisterDto)
+        {
+            var values = new ApplicationUser()
+            {
+                UserName = userRegisterDto.Username,
+                Email = userRegisterDto.Email,
+                Name = userRegisterDto.Name,
+                Surname = userRegisterDto.Surname,
+            };
+
+            var result= await _usermanager.CreateAsync(values, userRegisterDto.Password);
+
+            if(result.Succeeded)
+            {
+                return Ok("user added successfuly");
+            }
+            else
+            {
+                return Ok("Error occured. please try again");
+            }
+        }
+    }
+}
