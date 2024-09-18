887b0966de76c58d65eb1615877aad18eb756043
Dilek <dilekyal1@gmail.com>
2024-09-18 22:05:13 +0300

    feat: add identityServer

--- Services/Order/Infrastructure/MultiShop.Order.Persistence/Context/OrderContext.cs
+++ Services/Order/Infrastructure/MultiShop.Order.Persistence/Context/OrderContext.cs
@@ -12,7 +12,7 @@ namespace MultiShop.Order.Persistence.Context
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=localhost,1440;initial Catalog=MultiShopOrderDb;User=sa;Password=Dilek140521.");
+            optionsBuilder.UseSqlServer("Server=localhost,1440;initial Catalog=MultiShopOrderDb;User=sa;Password=123456aA*");
         }
 
         public DbSet<Address> Addresses { get; set; }
