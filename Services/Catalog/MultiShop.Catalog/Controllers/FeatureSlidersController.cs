3d33d68eeddd792ed6d9595dbf13beac25fd47af
Dilek <dilekyal1@gmail.com>
2024-11-25 23:03:34 +0300

    feat: add authentication and authorization operations

--- Services/Catalog/MultiShop.Catalog/Controllers/FeatureSlidersController.cs
+++ Services/Catalog/MultiShop.Catalog/Controllers/FeatureSlidersController.cs
@@ -1,11 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MultiShop.Catalog.Dtos.FeatureSliderDtos;
 using MultiShop.Catalog.Services.FeatureSliderServices;
 
 namespace MultiShop.Catalog.Controllers
 {
-    [Route("api/[controller]")]
+	[Authorize]
+	[Route("api/[controller]")]
     [ApiController]
     public class FeatureSlidersController : ControllerBase
     {
