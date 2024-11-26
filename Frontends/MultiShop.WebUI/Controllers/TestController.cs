d1bea780c0a274962d9aef051bfd0a4490511819
Dilek <dilekyal1@gmail.com>
2024-11-26 18:41:42 +0300

    feat: add discovery methods

--- Frontends/MultiShop.WebUI/Controllers/TestController.cs
+++ Frontends/MultiShop.WebUI/Controllers/TestController.cs
@@ -2,6 +2,7 @@
 using MultiShop.DtoLayer.CatalogDtos.CategoryDtos;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Net.Http.Headers;
 
 namespace MultiShop.WebUI.Controllers
 {
@@ -16,34 +17,36 @@ namespace MultiShop.WebUI.Controllers
 
 		public async Task<IActionResult> Index()
 		{
-			string token;
-			using(var httpClient=new HttpClient())
-			{
-				var request = new HttpRequestMessage
-				{
-					RequestUri = new Uri("http://localhost:5001/connect/token"),
-					Method = HttpMethod.Post,
-					Content=new FormUrlEncodedContent(new Dictionary<string, string>
-					{
-						{"client_id", "MultiShopVisitorId" },
-						{"client_secret", "multishopsecret" },
-						{"grant_type", "client_credentials" }
-					})
-				};
+            string token = "";
+            using (var httpClient = new HttpClient())
+            {
+                var request = new HttpRequestMessage
+                {
+                    RequestUri = new Uri("http://localhost:5001/connect/token"),
+                    Method = HttpMethod.Post,
+                    Content = new FormUrlEncodedContent(new Dictionary<string, string>
+                    {
+                        {"client_id","MultiShopVisitorId" },
+                        {"client_secret","multishopsecret" },
+                        {"grant_type","client_credentials" }
+                    })
+                };
 
-				using(var response = await httpClient.SendAsync(request))
-				{
-					if(response.IsSuccessStatusCode)
-					{
-						var content = await response.Content.ReadAsStringAsync();
-						var tokenResponse = JObject.Parse(content);
-						token = tokenResponse["access_token"].ToString();
-					}
-				}
-			}
+                using (var response = await httpClient.SendAsync(request))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+                        var tokenResponse = JObject.Parse(content);
+                        token = tokenResponse["access_token"].ToString();
+                    }
+                }
+            }
 
-			var client = _httpClientFactory.CreateClient();
-			var responseMessage = await client.GetAsync("https://localhost:7224/api/Categories");
+            var client = _httpClientFactory.CreateClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var responseMessage = await client.GetAsync("https://localhost:7224/api/Categories");
 			if (responseMessage.IsSuccessStatusCode)
 			{
 				var jsonData = await responseMessage.Content.ReadAsStringAsync();
@@ -52,5 +55,11 @@ namespace MultiShop.WebUI.Controllers
 			}
 			return View();
 		}
+
+        public IActionResult Deneme1()
+        {
+
+            return View();
+        }
 	}
 }
