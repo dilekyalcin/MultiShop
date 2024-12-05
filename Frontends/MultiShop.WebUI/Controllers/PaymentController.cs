aad297ff82ff5c316049a40843d32c5f299abb0a
Dilek <dilekyal1@gmail.com>
2024-12-06 01:18:15 +0300

    feat:  add discount, order and credit card operations

---
+++ Frontends/MultiShop.WebUI/Controllers/PaymentController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Index()
+        {
+            @ViewBag.directory1 = "MultiShop";
+            @ViewBag.directory2 = "Payment Screen";
+            @ViewBag.directory3 = "Payment by Card";
+            return View();
+        }
+    }
+}
